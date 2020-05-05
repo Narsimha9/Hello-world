c46aad70d4a1f9db51e3a9f1625f1ca8d3811fc2
Narsimha <narsimhulu.464@gmail.com>
2020-05-05 09:25:37 +0530

    sixth World

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ class HelloWorld
 	public static void main(String args[]) 
 	{ 
 		System.out.println("Hello, World"); 
-		System.out.println("5th world");
+		System.out.println("Sixth world");
 		System.out.println("Third World");
 	} 
 } 
